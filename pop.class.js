777db0dd52ec906a298ab361f98d892bf231a379
Jeroendevr <jeroendevries@runbox.eu>
2019-10-29 21:35:11 +0100

    Added function to check availability of spawns

--- pop.class.js
+++ pop.class.js
@@ -1,8 +1,15 @@
 class PopulationManager {
 
   constructor () {
-    console.log('Constructor called')
+    this.spawnAvailable = this._checkAvailable()
 
+
+    console.log("Spawn available " + this.spawnAvailable)
+
+  }
+
+  _checkAvailable () {
+    console.log('availability checked')
     if(Game.spawns['Spawn1'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
@@ -10,19 +17,25 @@ class PopulationManager {
             Game.spawns['Spawn1'].pos.x + 1,
             Game.spawns['Spawn1'].pos.y,
             {align: 'left', opacity: 0.8});
+
+        return false
+    } else {
+      return true
     }
   }
 
   spawn(role) {
+
     switch (role) {
       case 'harvester' :
-        console.log('Spwaning new harvester: ' + newName(role))
+        console.log('Spawning new harvester: ' + newName(role))
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName(role),
           {memory: {role: role}})
         break
 
       case 'builder' :
         console.log('Spwaning new builder: ' + newName(role))
+        break
 
       default:
         console.log("no role found")
@@ -36,5 +49,5 @@ class PopulationManager {
 module.exports = new PopulationManager()
 
 function newName(role){
-  var newName = role + Game.time
+  return role + Game.time
 }
