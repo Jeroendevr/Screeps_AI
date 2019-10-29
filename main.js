bab87d76cbacc61c1a6a956762790c9a2e0e130e
Jeroen de Vries <jeroendevries@MacMini.fritz.box>
2019-10-29 09:14:18 +0100

    integrated builder class

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder')
+
 
 module.exports.loop = function () {
 
@@ -33,6 +35,9 @@ module.exports.loop = function () {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
---
+++ role.class.js
@@ -0,0 +1,5 @@
+//Creating a class to keep track of amount of roles deployed
+
+
+
+module.exports = roleClass
